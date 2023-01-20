7b2285b6f137f1e3ab526293232f32ae6213921c
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-01-20 16:09:04 +0000

    Decouple from intecomm_lists (requires edc-constants >= 0.3.42)

--- intecomm_form_validators/subject/health_economics_form_validator.py
+++ intecomm_form_validators/subject/health_economics_form_validator.py
@@ -1,10 +1,17 @@
 from django import forms
-from edc_constants.constants import FREE_OF_CHARGE, OTHER, YES
+from edc_constants.constants import (
+    FREE_OF_CHARGE,
+    INSURANCE,
+    OTHER,
+    OWN_CASH,
+    PATIENT_CLUB,
+    RELATIVE,
+    YES,
+)
 from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
 from edc_dx.form_validators import DiagnosisFormValidatorMixin
 from edc_dx_review.utils import raise_if_clinical_review_does_not_exist
 from edc_form_validators import FormValidator
-from intecomm_lists.models import DrugPaySources
 
 
 class HealthEconomicsFormValidator(
@@ -112,7 +119,10 @@ class HealthEconomicsFormValidator(
             self.m2m_other_specify(
                 *[
                     obj.name
-                    for obj in DrugPaySources.objects.all()
+                    for obj in (
+                        # NOTE: Must align with choices in intecomm_lists.models.DrugPaySources
+                        (OWN_CASH, INSURANCE, PATIENT_CLUB, RELATIVE, FREE_OF_CHARGE, OTHER)
+                    )
                     if obj.name != FREE_OF_CHARGE
                 ],
                 m2m_field=f"rx_{cond}_paid_{duration}",
