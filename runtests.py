62a14d35907745c1ddb5c9db0bbb19774ff4c818
erikvw <ew2789@gmail.com>
2023-01-23 08:20:12 -0600

    fix duplicate key

--- runtests.py
+++ runtests.py
@@ -83,7 +83,6 @@ DEFAULT_SETTINGS = dict(  # nosec B106
     USE_TZ=True,
     STATIC_URL="/static/",
     DEFAULT_AUTO_FIELD="django.db.models.BigAutoField",
-    EDC_DX_LABELS=dict(hiv="HIV", dm="Diabetes", htn="Hypertension"),
 )
 
 
