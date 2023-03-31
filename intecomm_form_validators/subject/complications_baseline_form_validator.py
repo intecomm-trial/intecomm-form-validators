8a7beabfe3c349602b65d545cb185abf39ef142a
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-03-31 17:32:32 +0100

    Add form validator stubs for: Medications, ComplicationsBaseline, and OtherBaselineData

---
+++ intecomm_form_validators/subject/complications_baseline_form_validator.py
@@ -0,0 +1,6 @@
+from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
+from edc_form_validators import FormValidator
+
+
+class ComplicationsBaselineFormValidator(CrfFormValidatorMixin, FormValidator):
+    pass
