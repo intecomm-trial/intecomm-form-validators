e77e39ae9f0d191b7ba106e2e4beb2f2020e76a9
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-04-20 21:05:28 +0100

    Handle/catch exception raised by validate_total_days

--- intecomm_form_validators/subject/drug_refill_hiv_form_validator.py
+++ intecomm_form_validators/subject/drug_refill_hiv_form_validator.py
@@ -1,12 +1,17 @@
 from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
 from edc_form_validators import FormValidator
-from edc_rx.utils import validate_total_days
+from edc_rx.utils import TotalDaysMismatch, validate_total_days
 
 from .mixins import DrugRefillFormValidatorMixin
+
+TOTAL_DAYS_MISMATCH_ERROR = "TOTAL_DAYS_MISMATCH"
 
 
 class DrugRefillHivFormValidator(
     DrugRefillFormValidatorMixin, CrfFormValidatorMixin, FormValidator
 ):
     def clean(self):
-        validate_total_days(self, return_in_days=self.cleaned_data.get("return_in_days"))
+        try:
+            validate_total_days(self, return_in_days=self.cleaned_data.get("return_in_days"))
+        except TotalDaysMismatch as e:
+            self.raise_validation_error({"__all__": str(e)}, TOTAL_DAYS_MISMATCH_ERROR)
