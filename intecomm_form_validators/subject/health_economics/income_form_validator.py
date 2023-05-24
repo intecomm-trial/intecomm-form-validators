fc6bd166fbb9415b9c72be29bd78bb21eda810e2
erikvw <ew2789@gmail.com>
2023-05-23 22:04:38 -0500

    wip

--- intecomm_form_validators/subject/health_economics/income_form_validator.py
+++ intecomm_form_validators/subject/health_economics/income_form_validator.py
@@ -1,4 +1,6 @@
+from edc_constants.constants import MONTHLY, WEEKLY, YEARLY, YES
 from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
+from edc_crf.utils import raise_if_crf_review_does_not_exist
 from edc_dx_review.utils import raise_if_clinical_review_does_not_exist
 from edc_form_validators import FormValidator
 
@@ -9,3 +11,38 @@ class HealthEconomicsIncomeFormValidator(
 ):
     def clean(self):
         raise_if_clinical_review_does_not_exist(self.cleaned_data.get("subject_visit"))
+        raise_if_crf_review_does_not_exist(
+            self.cleaned_data.get("subject_visit"),
+            model="intecomm_subject.healtheconomicshouseholdhead",
+        )
+        raise_if_crf_review_does_not_exist(
+            self.cleaned_data.get("subject_visit"),
+            model="intecomm_subject.healtheconomicspatient",
+        )
+        raise_if_crf_review_does_not_exist(
+            self.cleaned_data.get("subject_visit"),
+            model="intecomm_subject.healtheconomicsassets",
+        )
+        raise_if_crf_review_does_not_exist(
+            self.cleaned_data.get("subject_visit"),
+            model="intecomm_subject.healtheconomicsproperty",
+        )
+
+        for fld in [
+            "wages",
+            "selling",
+            "rental_income",
+            "pension",
+            "ngo_assistance",
+            "interest",
+            "internal_remittance",
+            "external_remittance",
+        ]:
+            self.applicable_if(YES, field=fld, field_applicable=f"{fld}_value_known")
+            self.required_if(
+                WEEKLY,
+                MONTHLY,
+                YEARLY,
+                field=f"{fld}_value_known",
+                field_required=f"{fld}_value",
+            )
--- intecomm_form_validators/subject/health_economics/property_form_validator.py
+++ intecomm_form_validators/subject/health_economics/property_form_validator.py
@@ -1,4 +1,6 @@
+from edc_constants.constants import YES
 from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
+from edc_crf.utils import raise_if_crf_review_does_not_exist
 from edc_dx_review.utils import raise_if_clinical_review_does_not_exist
 from edc_form_validators import FormValidator
 
@@ -9,3 +11,23 @@ class HealthEconomicsPropertyFormValidator(
 ):
     def clean(self):
         raise_if_clinical_review_does_not_exist(self.cleaned_data.get("subject_visit"))
+        raise_if_crf_review_does_not_exist(
+            self.cleaned_data.get("subject_visit"),
+            model="intecomm_subject.healtheconomicshouseholdhead",
+        )
+        raise_if_crf_review_does_not_exist(
+            self.cleaned_data.get("subject_visit"),
+            model="intecomm_subject.healtheconomicspatient",
+        )
+        raise_if_crf_review_does_not_exist(
+            self.cleaned_data.get("subject_visit"),
+            model="intecomm_subject.healtheconomicsassets",
+        )
+        self.applicable_if(YES, field="land_owner", field_applicable="land_value_known")
+        self.required_if(YES, field="land_value_known", field_required="land_value")
+        self.applicable_if(
+            YES, field="land_additional", field_applicable="land_additional_known"
+        )
+        self.required_if(
+            YES, field="land_additional_known", field_required="land_additional_value"
+        )
