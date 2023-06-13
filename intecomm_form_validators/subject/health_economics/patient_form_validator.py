3e5220cb5ad2f026ea3ada27baa2e0d549db9cd8
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-06-13 20:37:35 +0100

    Refactor to use raise_if_crf_does_not_exist (from raise_if_crf_review_does_not_exist)

--- intecomm_form_validators/subject/health_economics/patient_form_validator.py
+++ intecomm_form_validators/subject/health_economics/patient_form_validator.py
@@ -1,6 +1,6 @@
 from edc_constants.constants import DONT_KNOW, NONE, OTHER
 from edc_crf.crf_form_validator_mixins import CrfFormValidatorMixin
-from edc_crf.utils import raise_if_crf_review_does_not_exist
+from edc_crf.utils import raise_if_crf_does_not_exist
 from edc_dx_review.utils import raise_if_clinical_review_does_not_exist
 from edc_form_validators import FormValidator
 
@@ -11,7 +11,7 @@ class HealthEconomicsPatientFormValidator(
 ):
     def clean(self):
         raise_if_clinical_review_does_not_exist(self.cleaned_data.get("subject_visit"))
-        raise_if_crf_review_does_not_exist(
+        raise_if_crf_does_not_exist(
             self.cleaned_data.get("subject_visit"),
             model="intecomm_subject.healtheconomicshouseholdhead",
         )
