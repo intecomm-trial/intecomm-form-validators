0dd1f4e0a0e03403927ff09de469d020f6cd9c68
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2024-11-18 10:54:46 +0000

    Fix tests failing with AttributeError raised due to introduction of `label_lower` property in django-mock-queries v2.3.0
    Tests raise:
    ```
    self._meta.label_lower = "intecomm_lists.dmtreatments"
        ^^^^^^^^^^^^^^^^^^^^^^
    
    AttributeError: property 'label_lower' of 'MockOptions' object has no setter
    ```
    
    Caused by MockModel lines like:
    
    `self._meta.label_lower = "intecomm_lists.dmtreatments"`
    
    Change to third party package here: https://github
    .com/stphivos/django-mock-queries/commit/87362006e10f99a7f2120780b2018126026fe3df

--- intecomm_form_validators/tests/mock_models.py
+++ intecomm_form_validators/tests/mock_models.py
@@ -5,14 +5,12 @@ class SiteMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "Site"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "sites.site"
 
 
 class SubjectScreeningMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "SubjectScreening"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_screening.subjectscreening"
 
     def __str__(self):
         return str(self.screening_identifier)
@@ -22,7 +20,6 @@ class PatientGroupMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "PatientGroup"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_screening.patientgroup"
         self.user_created = "frisco"
         self.user_modified = "frisco"
 
@@ -43,7 +40,6 @@ class PatientGroupRandoMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "PatientGroupRando"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_screening.patientgrouprando"
         self.user_created = "jasper"
         self.user_modified = "jasper"
 
@@ -64,7 +60,6 @@ class PatientLogMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "PatientLog"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_screening.patientlog"
 
     def __str__(self):
         return str(self.name)
@@ -80,28 +75,24 @@ class ConditionsMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "Conditions"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_list.condition"
 
 
 class ScreeningRefusalReasonsMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "ScreeningRefusalReasons"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_list.screeningrefusalreasons"
 
 
 class SocialHarmsMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "SocialHarms"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_subject.socialharms"
 
 
 class VitalsMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "Vitals"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_subject.vitals"
 
 
 class AppointmentMockModel(MockModel):
@@ -109,7 +100,6 @@ class AppointmentMockModel(MockModel):
         kwargs["mock_name"] = "Appointment"
         super().__init__(*args, **kwargs)
         self.visit_schedule_name = "visit_schedule"
-        self._meta.label_lower = "edc_appointment.appointment"
 
 
 class SubjectVisitMockModel(MockModel):
@@ -120,60 +110,51 @@ class SubjectVisitMockModel(MockModel):
         self.visit_schedule_name = appointment.visit_schedule_name
         self.schedule_name = appointment.schedule_name
         self.report_datetime = appointment.appt_datetime
-        self._meta.label_lower = "intecomm_subject.subjectvisit"
 
 
 class DmInitialReviewMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "DmInitialReview"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_subject.dminitialreview"
 
 
 class HivInitialReviewMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "HivInitialReview"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_subject.hivinitialreview"
 
 
 class HivReviewMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "HivReview"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_subject.hivreview"
 
 
 class HtnInitialReviewMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "HtnInitialReview"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_subject.htninitialreview"
 
 
 class DmTreatmentsMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "DmTreatments"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_lists.dmtreatments"
 
 
 class HtnTreatmentsMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "HtnTreatments"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_lists.htntreatments"
 
 
 class HealthEconomicsHouseholdHeadMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "HealthEconomicsHouseholdHead"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_subject.healtheconomicshouseholdhead"
 
 
 class InsuranceTypesMockModel(MockModel):
     def __init__(self, *args, **kwargs):
         kwargs["mock_name"] = "InsuranceTypes"
         super().__init__(*args, **kwargs)
-        self._meta.label_lower = "intecomm_lists.insurancetypes"
